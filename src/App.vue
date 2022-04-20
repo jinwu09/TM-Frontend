a16ceb2bd51427a835ee33951f7466dc27c579b8
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-04-20 11:14:32 +0800

    initial setup plugin
    popperjs, bootstrap, and sass

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,17 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 </script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it! test" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <router-link to="/test"> test</router-link>
       </nav>
     </div>
   </header>
@@ -24,9 +24,10 @@ import HelloWorld from '@/components/HelloWorld.vue'
 @import '@/assets/base.css';
 
 #app {
-  max-width: 1280px;
+/*   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
+  background-color: black; */
 
   font-weight: normal;
 }
