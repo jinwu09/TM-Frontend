8ca177da5040b28991cd58791f3af5cd8b667cf3
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-04-23 17:34:58 +0800

    Home View done

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
         <h1>Today's Menu</h1>
       </div>
       <nav>
-        <RouterLink to="/home">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Login" class="link">Login</RouterLink>
+        <button class="button"><RouterLink to="/singup">Register</RouterLink></button>
       </nav>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it! test" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <router-link to="/test"> test</router-link>
-      </nav>
-    </div> -->
   </header>
 <hr>
   <RouterView />
@@ -32,9 +21,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 @import '@/assets/base.css';
 
 #app {
-/*   max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;*/
+  /* max-width: max-content; */
+  /* margin: 0 auto; */
+  /* padding: 2rem; */
   
   font-weight: normal;
 }
@@ -59,15 +48,19 @@ header h1{
 
 
 @media (hover: hover) {
-  a:hover {
+  .link:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
   }
+}
+.button{
+  border-radius: 10px;
+  background-color: white;
+  transition: all 0.5s;
 }
 
 nav {
   width: auto;
   font-size: 12px;
-  text-align: center;
   margin-top: 2rem;
 }
 
@@ -78,18 +71,24 @@ nav a.router-link-exact-active {
 nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
-
+.button:hover{
+  background-color: hsla(160, 100%, 37%, 0.2);
+}
 nav a {
   display: inline-block;
-  padding: 0 1rem;
+  padding:  1rem;
   border-left: 1px solid var(--color-border);
+  text-decoration: none;
+  color: black;
+  transition: 0.5s;
+  border-radius: 10px;
 }
 
 nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+@media (min-width: 750px) {
   body {
     /* display: flex; */
     /* place-items: center; */
@@ -98,14 +97,14 @@ nav a:first-of-type {
   #app {
     /* display: grid;
     grid-template-columns: 1fr 1fr; */
-    padding: 0 2rem;
+    /* padding: 0 2rem; */
   }
 
   header {
     display: grid;
-    grid-template-columns: 3fr 1fr;
+    grid-template-columns: 1fr 1fr;
     gap: 1rem;
-    padding-left: calc(var(--section-gap) / 2);
+    padding: 0 calc(var(--section-gap) / 2) ;
     padding-top: 1rem;
   }
 
@@ -115,10 +114,9 @@ nav a:first-of-type {
   }
 
   nav {
-    text-align: left;
+    text-align: right;
     margin-left: -1rem;
     font-size: 1rem;
-
     padding: 1rem 0;
     margin-top: 1rem;
   }
---
+++ src/components/Welcome.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="flex content-size-100-600">
+        <div class="background_image content-size-100-600">s</div>
+        <div class="container">
+            <div class=" content">
+                <h2>Welcome to Today's Men</h2>
+                <p>Sample tagline</p>
+            </div>
+        </div>
+        <hr>
+        <div>
+
+        </div>
+    </div>
+</template>
+<style>
+.background_image {
+    position: absolute;
+    background-image: url("https://images.squarespace-cdn.com/content/v1/53b839afe4b07ea978436183/1608506169128-S6KYNEV61LEP5MS1UIH4/traditional-food-around-the-world-Travlinmad.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    /* filter: blur(10px); */
+    filter: brightness(0.75);
+}
+
+.content-size-100-600 {
+    width: 100%;
+    height: 600px;
+}
+
+.content {
+    position: absolute;
+    color: white;
+    transform: translateY(50%);
+    width: 500px;
+    word-spacing: 1px;
+    text-align: center;
+    font-size: 50px;
+}
+
+.content h2 {
+    font-size: 80px;
+}
+
+.gridcenter {
+    display: grid;
+    grid-template-columns: 1fr;
+}
+</style>
