6313106538d6903f99f1c8ab5e3610c96adb9bde
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-04-25 13:50:12 +0800

    update
    lels

---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="text" v-model="text" value="test">
+        <button @click="count++">You clicked me {{ count }} times.</button>
+        <p>testing output {{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+            text: ""
+        }
+    }
+}
+</script>
+
+<style>
+div {
+    border: black 1px solid;
+}
+</style>
