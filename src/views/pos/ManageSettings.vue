edc7a23c27ff5eb786d79eb5583982ad3aeedbd1
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-05-10 22:20:35 +0800

    Update ManageSettings.vue

--- src/views/pos/ManageSettings.vue
+++ src/views/pos/ManageSettings.vue
@@ -11,6 +11,7 @@ export default {
             companyinfo: [],
             Userinfo: [],
             isEdit: false,
+            isEditcomp: false,
             isSideActive: Boolean
         }
     },
@@ -21,21 +22,30 @@ export default {
         updateSettings() {
             this.$http.all([
                 this.$http.get('/api/auth/users/me/'),
-            ]).then(this.$http.spread((user) => {
+                this.$http.get('/api/company/' + this.store.getCompany().company_id)
+            ]).then(this.$http.spread((user, comp) => {
                 this.Userinfo = user.data
+                this.companyinfo = comp.data
             }))
         },
-        update(objarray) {
+        PutUserDB(objarray) {
             this.$http.put('/api/auth/users/me/', {
                 email: objarray.email,
                 first_name: objarray.first_name,
                 last_name: objarray.last_name
             }).then((res) => { this.updateSettings() }).catch((err) => { console.log(err) })
+        },
+        PutCompDB(objarray) {
+            this.$http.put('/api/company/' + this.store.getCompany().company_id + "/", {
+                user: objarray.user,
+                company_name: objarray.company_name,
+                company_address: objarray.company_address,
+                company_number: objarray.company_number
+            }).then((res) => { this.updateSettings() }).catch((err) => { console.log(err) })
         }
     },
     mounted() {
         this.updateSettings()
-        this.companyinfo = this.store.getCompany()
     }
 
 }
@@ -58,23 +68,49 @@ export default {
 
             </div>
         </div>
+        <!-- Company edits -->
         <div class="w-[80%]  mx-auto mt-11 shadow-2xl rounded-3xl p-10 ">
+            <p class="text-2xl"> Company Info </p>
+            <hr class="my-4">
             <div class="grid grid-cols-3 gap-y-4 ">
 
-                <p>Emai :</p>
+                <p class="my-auto">Company Name :</p>
+                <input class="input col-span-2" type="text" :disabled="isEditcomp == false"
+                    v-model="companyinfo.company_name">
+                <p class="my-auto">Company Address :</p>
+                <input class="input col-span-2" type="text" :disabled="isEditcomp == false"
+                    v-model="companyinfo.company_address">
+                <p class="my-auto">Company Number :</p>
+                <input class="input col-span-2" type="text" :disabled="isEditcomp == false"
+                    v-model="companyinfo.company_number">
+            </div>
+            <div class="flex mt-5">
+                <button class="linkbutton ml-auto bg-secondary text-white px-5" @click="isEditcomp = !isEditcomp">Edit
+                    Profile</button>
+                <button class="linkbutton bg-secondary text-white px-5" :class="{ disable: !isEditcomp }"
+                    :disabled="isEditcomp == false" @click="PutCompDB(companyinfo), isEditcomp = false">Update</button>
+            </div>
+        </div>
+        <!-- user edits -->
+        <div class="w-[80%]  mx-auto mt-11 shadow-2xl rounded-3xl p-10 ">
+            <p class="text-2xl"> User Info </p>
+            <hr class="my-4">
+            <div class="grid grid-cols-3 gap-y-4 ">
+
+                <p class="my-auto">Emai :</p>
                 <input class="input col-span-2" type="email" :disabled="isEdit == false" v-model="Userinfo.email">
-                <p>Username:</p>
+                <p class="my-auto">Username:</p>
                 <input class="input col-span-2" type="text" :disabled="isEdit == false" v-model="Userinfo.username">
-                <p>First Name :</p>
+                <p class="my-auto">First Name :</p>
                 <input class="input col-span-2" type="text" :disabled="isEdit == false" v-model="Userinfo.first_name">
-                <p>Last Name :</p>
+                <p class="my-auto">Last Name :</p>
                 <input class="input col-span-2" type="text" :disabled="isEdit == false" v-model="Userinfo.last_name">
             </div>
             <div class="flex mt-5">
                 <button class="linkbutton ml-auto bg-secondary text-white px-5" @click="isEdit = !isEdit">Edit
                     Profile</button>
                 <button class="linkbutton bg-secondary text-white px-5" :class="{ disable: !isEdit }"
-                    :disabled="isEdit == false" @click="update(Userinfo), isEdit = false">Update</button>
+                    :disabled="isEdit == false" @click="PutUserDB(Userinfo), isEdit = false">Update</button>
             </div>
         </div>
     </div>
