74bf3d433a4374d42ffcd8827a403e4f1b23c383
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-05-23 15:43:14 +0800

    final??
    final??

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,13 +4,13 @@
         <img class="bg-Welcome w-full h-[38rem] brightness-[.6] object-cover"
             src="https://images.squarespace-cdn.com/content/v1/53b839afe4b07ea978436183/1608506169128-S6KYNEV61LEP5MS1UIH4/traditional-food-around-the-world-Travlinmad.jpg"
             alt="">
-        <div class="absolute left-[20%] top-1/4 text-white  w-72 text-center ">
-            <div class="">
-                <div class="text-6xl">
-                    <h2>Welcome to Today's Men</h2>
+        <div class="absolute left-[20%] top-1/4 text-white   text-center ">
+            <div class=" grid grid-cols-1 ">
+                <div class="text-6xl w-72 mx-auto">
+                    <h2>Welcome to Today's Menu</h2>
                 </div>
-                <div class="text-4xl">
-                    <p>Sample tagline</p>
+                <div class="text-2xl w-80">
+                    <p>We’re always in the mood for food!</p>
 
                 </div>
             </div>
